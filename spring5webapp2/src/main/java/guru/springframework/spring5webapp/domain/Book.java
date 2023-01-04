cbec787e94b26d791e6f5dc4342ccf8b07b30c1f
trkck <trk.kck95@gmail.com>
2023-01-04 21:44:13 +0300

    Added equals hash code and toString metods

--- spring5webapp2/src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ spring5webapp2/src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,11 +1,23 @@
 package guru.springframework.spring5webapp.domain;
 
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
 
+                // we have here a relationship between books and author many to many we should certain with ManyToMany annotation
+    @ManyToMany
+    @JoinTable(name = "author_book",joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> authors;
 
     public Book() {
@@ -14,6 +26,14 @@ public class Book {
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -38,5 +58,30 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
