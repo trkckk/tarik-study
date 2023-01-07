16810d0f455aa9ae4a18f662250c4bc0c31c0693
trkck <trk.kck95@gmail.com>
2023-01-08 00:01:10 +0300

    added bootstrap class and used first time @Component annotation

--- spring5webapp2/src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ spring5webapp2/src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -3,6 +3,7 @@ package guru.springframework.spring5webapp.domain;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -18,7 +19,7 @@ public class Book {
     @ManyToMany
     @JoinTable(name = "author_book",joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name="author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Book() {
     }
